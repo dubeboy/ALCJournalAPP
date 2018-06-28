0cf120b8ead30ef50086b035ded33d1fee62e831
dubeboy <216004854@student.uj.ac.za>
2018-06-28 20:12:36 +0200

    save journal

--- app/src/main/java/za/co/dubedivine/journalapp/ui/addJournal/AddJournal.kt
+++ app/src/main/java/za/co/dubedivine/journalapp/ui/addJournal/AddJournal.kt
@@ -1,31 +1,68 @@
 package za.co.dubedivine.journalapp.ui.addJournal
 
+import android.arch.lifecycle.ViewModelProviders
 import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.add_journal_activity.*
 import za.co.dubedivine.journalapp.R
+import za.co.dubedivine.journalapp.database.JournalEntry
+import java.util.*
 
 class AddJournal : AppCompatActivity() {
 
     companion object {
 
+        const val TAG = "AddJournal"
+
         @JvmStatic
         fun getStartIntent(context: Context): Intent {
-            return Intent(context, AddJournal::class.java)
+            val intent = Intent(context, AddJournal::class.java)
+            return intent
         }
 
     }
+
+    private lateinit var viewModel: AddJournalViewModel
+    private val createdAt: Date = Date()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_journal_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, AddJournalFragment.newInstance())
-                    .commitNow()
-        }
+        viewModel = ViewModelProviders.of(this).get(AddJournalViewModel::class.java)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        MenuInflater(this).inflate(R.menu.menu_add_journal, menu)
+        return true
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.menu_item_done -> {
+                // save and then finish
+                viewModel.insertJournal(createJournalEntityToSave())
+                finish()
+            }
+        }
+       return true
+    }
+
+    // need to save when the activity is paused
+
+    private fun createJournalEntityToSave(): JournalEntry {
+        val body = et_journal_body.text.toString()
+        val title = et_journal_title.text.toString()
+        return JournalEntry(title, body, Date(), createdAt)
+    }
+
+    override fun onBackPressed() {
+        viewModel.insertJournal(createJournalEntityToSave())
+        super.onBackPressed()
+    }
 }
--- app/src/main/java/za/co/dubedivine/journalapp/ui/addJournal/AddJournalViewModel.kt
+++ app/src/main/java/za/co/dubedivine/journalapp/ui/addJournal/AddJournalViewModel.kt
@@ -2,18 +2,14 @@ package za.co.dubedivine.journalapp.ui.addJournal
 
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
+import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.ViewModel
 import za.co.dubedivine.journalapp.database.AppDatabase
 import za.co.dubedivine.journalapp.database.JournalDao
 import za.co.dubedivine.journalapp.database.JournalEntry
 
-class AddJournalViewModel(application: Application) : AndroidViewModel(application) {
-    private val journalDao: JournalDao
-
-    init {
-        val database = AppDatabase.getInstance(this.getApplication())
-        journalDao = database.journalDao()
-    }
+class AddJournalViewModel(private val database: AppDatabase) : ViewModel() {
+    private val journalDao: JournalDao = database.journalDao()
 
     fun insertJournal(journal: JournalEntry) {
         journalDao.insert(journal)
@@ -22,6 +18,4 @@ class AddJournalViewModel(application: Application) : AndroidViewModel(applicati
     fun updateJournal(journal: JournalEntry) {
         journalDao.insert(journal)
     }
-
-
 }
