fffc517276d74e63a63567bdd2e5e79bec87732b
dubeboy <216004854@student.uj.ac.za>
2018-06-30 02:28:20 +0200

    add setting activity

--- app/src/main/java/za/co/dubedivine/journalapp/ui/main/JournalAdapter.kt
+++ app/src/main/java/za/co/dubedivine/journalapp/ui/main/JournalAdapter.kt
@@ -1,6 +1,8 @@
 package za.co.dubedivine.journalapp.ui.main
 
+import android.content.res.Resources
 import android.provider.Settings.System.DATE_FORMAT
+import android.support.v4.content.res.ResourcesCompat
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -26,7 +28,7 @@ class JournalAdapter : RecyclerView.Adapter<JournalAdapter.JournalViewHolder>() 
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JournalViewHolder {
-        val view = LayoutInflater.from(parent?.context).inflate(R.layout.journal_layout, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.journal_layout, parent, false)
         return JournalViewHolder(view)
     }
 
@@ -34,7 +36,7 @@ class JournalAdapter : RecyclerView.Adapter<JournalAdapter.JournalViewHolder>() 
 
     // default 16 items
     override fun onBindViewHolder(holder: JournalViewHolder, position: Int) {
-        holder?.bind(journals[position])
+        holder.bind(journals[position])
     }
 
     fun addJournal(journalEntry: JournalEntry) {
@@ -47,13 +49,31 @@ class JournalAdapter : RecyclerView.Adapter<JournalAdapter.JournalViewHolder>() 
         private val tvTitle: TextView = view.findViewById(R.id.tv_title)
         private val tvBody: TextView = view.findViewById(R.id.tv_body)
         private val tvDate: TextView = view.findViewById(R.id.tv_modified_on)
+        private val tvBar: TextView = view.findViewById(R.id.tv_bar)
 
         fun bind(journalEntry: JournalEntry) {
             tvTitle.text = journalEntry.title
             tvBody.text = journalEntry.body
             tvDate.text = getSimpleDateFormatter().format(journalEntry.modifiedAt)
+           // tvBar.setBackgroundColor(genBarColor(journalEntry))
             itemView.setOnClickListener {
                 itemView.context.startActivity(ViewJournal.getStartIntent(itemView.context, journalEntry.id!!))
+            }
+        }
+
+        private fun genBarColor(mood: Int): Int {
+           return when (mood) {
+                2 -> {
+                    Resources.getSystem().getColor(R.color.happy_feeling_color)
+
+                }
+                3 -> {
+                    Resources.getSystem().getColor(R.color.sad_feeling_color)
+                }
+               else -> {
+                   //todo fix
+                   Resources.getSystem().getColor(R.color.neutral_feeling_color)
+               }
             }
         }
 
